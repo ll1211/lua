2651a8a642747ea6040e748c2b41e12671ac3d13
ll1211 <liuliang1211@163.com>
2015-12-25 17:55:29 +0800

    add

---
+++ lua0201/src/luaArr.lua
@@ -0,0 +1,9 @@
+arr = {1,2,3,4,"liuliang"}
+
+for key, var in pairs(arr) do
+	print(key,var)
+end
+
+for var in arr do
+	print(var)
+end
---
+++ lua0201/src/main.lua
@@ -0,0 +1,5 @@
+--print("hello lua")
+
+for var=1, 100 do
+	print(var)
+end
